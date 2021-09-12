c2d5b7374dc30a8a676b49290c7751824e8ac339
latorreandrea <latorre.andrea.93@gmail.com>
2021-09-12 09:57:12 +0000

    set test for objects

---
+++ assets/script/tests/game.test.js
@@ -0,0 +1,30 @@
+  /**
+   * @jest-environment jsdom
+   */
+
+  const {
+    game
+  } = require("../game");
+
+  beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "UTF-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+  });
+
+  describe("game object contains correct keys", () => {
+    test("score key exist", () => {
+      expect("score" in game).toBe(true);
+    });
+    test("currentGame key exist", () => {
+      expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exist", () => {
+      expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exist", () => {
+      expect("choices" in game).toBe(true);
+    });
+  });
